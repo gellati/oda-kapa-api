d425165ccf323fff9ea7d4beadc0788a20c0cc94
Omar El-Begawy <omar.elbegawy@gmail.com>
2017-08-07 23:03:07 +0300

    rearranged files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
 //var gulp = require('gulp')
 
 var requireDir = require('require-dir')
-requireDir('./gulp_tasks')
+requireDir('./js/gulp_tasks')
--- oda-kapa-express/gulp_tasks/default.js
+++
@@ -1,5 +0,0 @@
-var gulp = require('gulp')
-
-gulp.task("default", function(){
-  gulp.watch(["kapa-api/kapa-api.yaml", "index.html"], ["swagger"])
-});
